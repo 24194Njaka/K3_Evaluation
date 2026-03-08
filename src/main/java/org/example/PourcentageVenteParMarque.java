ec5ccd9da681e6107170d050bae6eb600ea08032
Njaka <hei.herinjaka@gmail.com>
2026-03-08 10:52:06 +0300

    feat: add PourcentageVenteParMarque

---
+++ src/main/java/org/example/PourcentageVenteParMarque.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class PourcentageVenteParMarque {
+    private Marque marque;
+    private String pourcentage;
+
+    public Marque getMarque() { return marque; }
+    public void setMarque(String marque) {
+        this.marque = Marque.valueOf(marque);
+    }
+
+    public String getPourcentage() { return pourcentage; }
+    public void setPourcentage(String pourcentage) {
+        this.pourcentage = pourcentage;
+    }
+}
