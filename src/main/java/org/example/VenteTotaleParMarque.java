f222d84023d8e228ed6f1026cd2b1750067ee165
Njaka <hei.herinjaka@gmail.com>
2026-03-08 10:27:37 +0300

    feat: add class VenteTotaleParMarque

---
+++ src/main/java/org/example/VenteTotaleParMarque.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class VenteTotaleParMarque {
+    private int nbrePieceKia;
+    private int nbrePieceDaewoo;
+
+    public int getNbrePieceKia() { return nbrePieceKia; }
+    public void setNbrePieceKia(int nbrePieceKia) {
+        this.nbrePieceKia = nbrePieceKia;
+    }
+
+    public int getNbrePieceDaewoo() { return nbrePieceDaewoo; }
+    public void setNbrePieceDaewoo(int nbrePieceDaewoo) {
+        this.nbrePieceDaewoo = nbrePieceDaewoo;
+    }
+}
